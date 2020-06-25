34b6abdebfc27be16da63a20ea967c3fd10d9296
johanIbtaten <j.ibtaten@hotmail.fr>
2020-06-25 11:32:10 +0200

    personal pipe

--- src/app/library/components/table-light/table-light.component.ts
+++ src/app/library/components/table-light/table-light.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Order } from 'src/app/shared/models/order';
 @Component({
   selector: 'app-table-light',
   templateUrl: './table-light.component.html',
@@ -7,11 +6,8 @@ import { Order } from 'src/app/shared/models/order';
 })
 export class TableLightComponent implements OnInit {
   @Input() headers: string[];
-  @Input() collection: Order[];
   constructor() {
-    console.log(this.collection);
   }
   ngOnInit(): void {
-    console.log(this.collection);
   }
 }
